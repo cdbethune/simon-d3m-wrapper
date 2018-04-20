2c0e8db75d37f53c78e371cbb652b9baad4cf439
Ubuntu <paul@newknowledge.io>
2018-04-20 19:14:40 +0000

    changed to reflect current API change in d3m module, 21, fixing version to 1.1.0 to match simon

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='SimonD3MWrapper',
-    version='1.0.0',
+    version='1.1.0',
     description='A thin client for interacting with dockerized simon primitive',
     packages=['SimonD3MWrapper'],
     install_requires=["numpy",
