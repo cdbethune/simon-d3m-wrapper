1cb2102a731c3c2f1e8ef93bab7d63bb3b6dd3fa
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2019-01-07 22:50:53 +0000

    edit produce function

--- SimonD3MWrapper/wrapper.py
+++ SimonD3MWrapper/wrapper.py
@@ -234,7 +234,7 @@ class simon(TransformerPrimitiveBase[Inputs, Outputs, Hyperparams]):
             # semantic types
             # overwrite with SIMON annotation of highest probability
             # TODO: add functionality to sample from probabilities??
-            annotation = simon_annotations['semantic types'][i].index(max(simon_annotations['probabilties'][i]))
+            annotation = simon_annotations['semantic types'][i].index(max(simon_annotations['probabilities'][i]))
             semantic_type = metadata['semantic_type']
             if overwrite or semantic_type is "" or semantic_type is None or 'semantic_type' not in metadata.keys():           
                 if annotation == 'categorical':
