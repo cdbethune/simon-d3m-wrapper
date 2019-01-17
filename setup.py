ca95c7885d7d8c6613fbb13cd10a0a6404c963d9
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2019-01-17 23:00:56 +0000

    update dependencies

--- setup.py
+++ setup.py
@@ -4,10 +4,9 @@ setup(name='SimonD3MWrapper',
     version='1.2.1',
     description='A thin client for interacting with dockerized simon primitive',
     packages=['SimonD3MWrapper'],
-    install_requires=["numpy",
-        "pandas",
-        "requests",
-        "typing",
+    install_requires=["numpy==1.15.4",
+        "pandas==0.23.4",
+        "typing==3.6.6",
         "Simon==1.2.1"],
     dependency_links=[
         "git+https://github.com/NewKnowledge/simon@d559abda8137fd6e351137a2a66d0464f67b718e#egg=Simon-1.2.1"
